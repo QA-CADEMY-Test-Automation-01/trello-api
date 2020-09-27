babb185b47c91f8140ec535ace0db1a673e184f0
Jose Colina Biracochea <Jose.Colina@jalasoft.com>
2020-09-26 20:10:05 -0400

    webdriver manager configured

--- src/test/java/trello/ui/main.java
+++ src/test/java/trello/ui/main.java
@@ -1,2 +1,25 @@
-package trello.ui;public class main {
+package trello.ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class main {
+    public static void main(String[] args){
+        //Configure driver manually
+//        System.setProperty("webdriver.chrome.driver", "d:\\otros\\webUI\\chromedriver_win32\\chromedriver.exe");
+        //Configure web driver manager
+        WebDriverManager.firefoxdriver().setup();
+
+        //Create web driver
+        //Open browser
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.ebay.com/");
+
+        //Close browser
+//        driver.close();
+        //Close browser and kill process
+        driver.quit();
+    }
 }
